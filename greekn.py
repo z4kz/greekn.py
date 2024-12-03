7b8a82763dabfdc312033f1f32bd7e5249075723
zak storer <92131024+z4kz@users.noreply.github.com>
2024-12-03 04:13:16 -0700

    create greekn.py

---
+++ greekn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Print the Greek number system."""
+
+print('α   β   γ   δ   ε   ϝ   ζ   η   θ')
+print('1   2   3   4   5   6   7   8   9')
+print()
+print('ι   κ   λ   μ   ν   ξ   ο   π   ϙ')
+print('10  20  30  40  50  60  70  80  90')
+print()
+print('ρ   σ   τ   υ   φ   χ   ψ   ω   ϡ')
+print('100 200 300 400 500 600 700 800 900')
+print()
+print(',α   ,β   ,γ   ,δ   ,ε   ,ϝ   ,ζ   ,η   ,θ')
+print('1000 2000 3000 4000 5000 6000 7000 8000 9000')
